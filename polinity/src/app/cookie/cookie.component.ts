9e52936d50ea1479610af1699e9fef34ad405e87
Fatih Yıldırım <66695748+fthyildirim@users.noreply.github.com>
2023-02-24 00:19:29 +0300

    day over

---
+++ polinity/src/app/cookie/cookie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-cookie',
+  templateUrl: './cookie.component.html',
+  styleUrls: ['./cookie.component.scss']
+})
+export class CookieComponent {
+
+
+}
--- polinity/src/app/users/users.component.ts
+++ polinity/src/app/users/users.component.ts
@@ -1,8 +1,9 @@
+import { ApiModel } from './../model/api.model';
 import { CharacterModel } from './../model/character.model';
 import { UsersService } from './../users.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, filter, map, mergeMap, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, map, mergeMap, switchMap, take, tap } from 'rxjs/operators';
 import { iif, of, Observable, from, throwError } from 'rxjs';
 import { PageableInfoModel } from '../model/characters.response';
 
@@ -12,6 +13,7 @@ import { PageableInfoModel } from '../model/characters.response';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  searchText: any;
   constructor(private usersService: UsersService) {}
   search = new FormControl("");
   title = 'pagination';
@@ -20,20 +22,20 @@ export class UsersComponent implements OnInit {
   page: number = 1;
   count: number = 0;
   tableSize: number = 20;
-  
+  message=""
+
   ngOnInit(): void {
     this.usersService.getApi().pipe(
       switchMap(() => {
         return this.getCharacters();
       }),
     ).subscribe();
-    // this.PostsOne();
     this.search.valueChanges
     .pipe(debounceTime(500),
     map(val => val?.trim()),
     distinctUntilChanged(),
-    mergeMap(val => iif(() => (val || '').length > 3, of(val), throwError(() => val))),
-     mergeMap(val => this.getCharacters({name: val}))
+    // mergeMap(val => iif(() => (val || '').length > 1, of(val), throwError(() => val))),
+    mergeMap(val => this.getCharacters({name: val}))
     )
     .subscribe();
   }
@@ -52,4 +54,11 @@ export class UsersComponent implements OnInit {
   getCharactersByUrl(url?: string | null) {
     this.getCharacters({}, url).subscribe();
   }
+  addItem(value: any){
+    // this.usersService.getApi().pipe(
+    //   push({id:value, name:value})
+    // )
+
+  }
 }
+
